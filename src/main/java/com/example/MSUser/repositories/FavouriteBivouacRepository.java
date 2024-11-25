32b8b0a27040f96b74efcb29520e2bebaf89f42d
NathBel <bellus.nathan@gmail.com>
2024-11-25 11:59:08 +0100

    :ambulance: fix: field name error

--- src/main/java/com/example/MSUser/repositories/FavouriteBivouacRepository.java
+++ src/main/java/com/example/MSUser/repositories/FavouriteBivouacRepository.java
@@ -3,6 +3,8 @@ package com.example.MSUser.repositories;
 import com.example.MSUser.models.FavouriteBivouacId;
 import com.example.MSUser.models.FavouriteBivouacs;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +12,8 @@ import java.util.List;
 @Repository
 public interface FavouriteBivouacRepository extends JpaRepository<FavouriteBivouacs, FavouriteBivouacId> {
 
-    public List<FavouriteBivouacs> findAllByUser_UserId(Long userId);
+    @Query("SELECT f FROM favourite_bivouacs f WHERE f.user.user_id = :userId")
+    public List<FavouriteBivouacs> findAllByUserId(@Param("userId") Long userId);
+
 
 }
